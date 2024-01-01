cbe1ac4a3dd7e59c041866a0b471587215c534b5
楊朝鈞 <paul@keroro-MacBook-Pro.local>
2024-01-01 15:27:49 +0800

    first

--- test.py
+++
@@ -1,8 +0,0 @@
-data = []
-with open("./data/pvuv.txt") as fin:
-    for line in fin:
-        line = line[:-1]
-        pdate, pv, uv = line.split("\t")
-        data.append((pdate, pv, uv))
-
-type(data)
